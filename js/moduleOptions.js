9c34f3c2e3d85c28efad0d21fcef78e5c3fe22f5
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-02 16:24:25 +1300

    new style / optimize pages getElementById instead of getElementsByName

--- js/moduleOptions.js
+++ js/moduleOptions.js
@@ -1,17 +1,17 @@
 import { CONFIG } from "./exports/exports.js";
 
 const DOM = {
-  DEBUG_OPTION: document.getElementsByName(CONFIG.SETTINGS.DEBUG),
-  ENCRYPTED_MEDIA_OPTION: document.getElementsByName(CONFIG.SETTINGS.ENCRYPTED_MEDIA),
+  DEBUG_OPTION: document.getElementById(CONFIG.SETTINGS.DEBUG),
+  ENCRYPTED_MEDIA_OPTION: document.getElementById(CONFIG.SETTINGS.ENCRYPTED_MEDIA),
   COLLAPSIBLES: document.getElementsByClassName("collapsible")
 }
 
-DOM.DEBUG_OPTION[0].addEventListener("click", (e) => {
+DOM.DEBUG_OPTION.addEventListener("click", (e) => {
   window.localStorage.setItem(CONFIG.SETTINGS.DEBUG, e.target.checked);
   browser.runtime.sendMessage({ requestForStateUpdate: true }); // OptionsModule -> BackgroundModule
 })
 
-DOM.ENCRYPTED_MEDIA_OPTION[0].addEventListener("click", (e) => {
+DOM.ENCRYPTED_MEDIA_OPTION.addEventListener("click", (e) => {
   window.localStorage.setItem(CONFIG.SETTINGS.ENCRYPTED_MEDIA, e.target.checked);
 })
 
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const debugEnabled = window.localStorage.getItem(CONFIG.SETTINGS.DEBUG);
   const encryptedMediaDisabled = window.localStorage.getItem(CONFIG.SETTINGS.ENCRYPTED_MEDIA);
 
-  DOM.DEBUG_OPTION[0].checked = debugEnabled !== null ? debugEnabled == "true" : false;
-  DOM.ENCRYPTED_MEDIA_OPTION[0].checked = encryptedMediaDisabled !== null ? encryptedMediaDisabled === "true" : true;
+  DOM.DEBUG_OPTION.checked = debugEnabled !== null ? debugEnabled == "true" : false;
+  DOM.ENCRYPTED_MEDIA_OPTION.checked = encryptedMediaDisabled !== null ? encryptedMediaDisabled === "true" : true;
 });
 
 for (var i = 0; i < DOM.COLLAPSIBLES.length; i++) {
--- js/modulePopup.js
+++ js/modulePopup.js
@@ -2,36 +2,39 @@ import { CONFIG, STATE, requestState, logDebug } from "./exports/exports.js";
 import { debounceEvent } from "./exports/debounce.js";
 
 const DOM = {
-  CONTENT_RULES: document.getElementsByName(CONFIG.SETTINGS.FRAGMENTS)
+  CONTENT_RULES: document.getElementById(CONFIG.SETTINGS.FRAGMENTS)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   requestState("popupModule", () => {
     if (!STATE.enabled) {
-      DOM.CONTENT_RULES[0].disabled = true;
+      DOM.CONTENT_RULES.disabled = true;
     }
     else {
-      DOM.CONTENT_RULES[0].value = STATE.fragments_storage;
+      DOM.CONTENT_RULES.value = STATE.fragments_storage;
     }
   });
 });
 
 window.addEventListener('unload', function () {
-  try {
-    const capture = DOM.CONTENT_RULES[0].value;
+  if (STATE.enabled) {
+    try {
+      const capture = DOM.CONTENT_RULES.value;
 
-    if (capture != undefined && capture.length > 1) {
-      window.localStorage.setItem(CONFIG.SETTINGS.FRAGMENTS, DOM.CONTENT_RULES[0].value);
-      browser.runtime.sendMessage({ requestForStateUpdate: true }); // PopupModule -> BackgroundModule
+      if (capture != undefined && capture.length > 1) {
+        window.localStorage.setItem(CONFIG.SETTINGS.FRAGMENTS, DOM.CONTENT_RULES.value);
+        browser.runtime.sendMessage({ requestForStateUpdate: true }); // PopupModule -> BackgroundModule
+      }
+
+    } catch {
+      logDebug("popupModule::domDestroyed");
     }
-  } catch {
-    logDebug("popupModule::domDestroyed");
   }
 });
 
-if (DOM.CONTENT_RULES.length > 0) {
-  DOM.CONTENT_RULES[0].addEventListener("input", debounceEvent((e) => {
+DOM.CONTENT_RULES.addEventListener("input", debounceEvent((e) => {
+  if (STATE.enabled) {
     e.target.value !== "" ? window.localStorage.setItem(CONFIG.SETTINGS.FRAGMENTS, e.target.value) : window.localStorage.removeItem(CONFIG.SETTINGS.FRAGMENTS);
     browser.runtime.sendMessage({ requestForStateUpdate: true }); // PopupModule -> BackgroundModule
-  }, 750));
-}
+  }
+}, 750));
