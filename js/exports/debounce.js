49d93b3a718e4b96a7f51051c866a94106f8a69b
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-04 17:49:06 +1300

    jsdocs

--- js/exports/debounce.js
+++ js/exports/debounce.js
@@ -1,7 +1,30 @@
-export function debounceEvent(func, delay) {
+/**
+ * Creates a debounced version of a function that delays its execution 
+ * until after a specified delay period has elapsed since the last time 
+ * it was invoked.
+ *
+ * @param {Function} func - The function to debounce.
+ * @param {number} delay - The number of milliseconds to delay.
+ * @returns {Function} A new debounced function that, when invoked, 
+ *                    will delay the execution of `func` until after 
+ *                    `delay` milliseconds have passed since the last 
+ *                    time it was invoked.
+ *
+ * @example
+ * const debouncedFunction = debounceEvent(() => {
+ *     console.log('Function executed!');
+ * }, 200);
+ *
+ * // The following calls will only execute the function once, 
+ * // 200 milliseconds after the last call.
+ * debouncedFunction();
+ * debouncedFunction();
+ * debouncedFunction();
+ */
+function debounceEvent(func, delay) {
   let timeoutId;
   return function (...args) {
-    timeoutId && clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(this, args), delay);
+      timeoutId && clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func.apply(this, args), delay);
   };
 }
--- js/exports/search.js
+++ js/exports/search.js
@@ -1,3 +1,12 @@
+/**
+ * Searches for the last occurrence of a specified word in a given string, 
+ * starting from the end of the string.
+ *
+ * @param {string} str - The string in which to search for the word.
+ * @param {string} word - The word to search for in the string.
+ * @returns {number|null} The index of the last occurrence of the word in the string, 
+ *                       or null if the word is not found.
+ */
 export function searchFromEnd(str, word) {
   const wordLenMinusOne = word.length - 1;
   let s = 0;
