b2f6aa3f8d9d9228d4baa3f4ab91fbcced3b4781
FirstTimeEZ <179362031+FirstTimeEZ@users.noreply.github.com>
2024-11-04 17:32:08 +1300

    searchFromEnd

--- js/exports/search.js
+++ js/exports/search.js
@@ -1,10 +1,13 @@
 export function searchFromEnd(str, word) {
   const wordLenMinusOne = word.length - 1;
-
+  let s = 0;
   for (let index = str.length - 1; index > 0; index--) {
     if (str[index] === word[wordLenMinusOne]) {
-      if (str.substring(index - wordLenMinusOne, index + 1) === word) {
-        return index;
+      for (let innerIndex = 0; innerIndex < word.length; innerIndex++) {
+        (word[wordLenMinusOne - innerIndex] == str[index - innerIndex]) ? (s++) : (s = 0);
+        if (s == word.length) {
+          return index;
+        }
       }
     }
   }
